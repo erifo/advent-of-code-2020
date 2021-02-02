22518299e8cc359119101f4db3691e8f2b00166c
erifo <eric.forsberg.work@gmail.com>
2021-02-03 00:48:50 +0100

    Day 8 setup

---
+++ day08/main.py
@@ -0,0 +1,17 @@
+def load(filename):
+    f = open(filename, "r")
+    lines = f.readlines()
+    return lines
+
+def solve_a(data):
+    pass
+
+def solve_b(data):
+    pass
+
+def main():
+    data = load("data.txt")
+    print(solve_a(data)) #
+    print(solve_b(data)) #
+
+main()
